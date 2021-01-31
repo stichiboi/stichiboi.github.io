f40615eb879a5d3032d270f17e54373800f2345b
stichiboi <stichiboi@gmail.com>
2021-01-31 17:26:19 +0100

    Improved quotes functionality + minor CSS fixes - Immediate swap - Can trigger swap on click

--- stichiclock/src/ts/quotes.ts
+++ stichiclock/src/ts/quotes.ts
@@ -1,25 +1,36 @@
 const quotes = require('../assets/quotes.json').quotes;
 
+const quoteDisplay = $('#quote');
+const quoteContainer = $('#quote-container .back-contrast');
+
 (function () {
     const changeQuoteInterval = 10000; //in ms
-    const quoteDisplay = $('#quote');
-    const quoteContainer = $('#quote-container .back-contrast')[0];
-
-    setInterval(() => {
-        const currId = quoteDisplay.attr('data-quote-id');
-
-        const id = Math.floor(Math.random() * quotes.length);
-        if(id.toString() !== currId){
-            const quote = `${quotes[id].quote} - ${quotes[id].author}`;
-            quoteDisplay.fadeOut(() => {
-                quoteContainer.style.width = `min(${quote.length * 10}px, 80vw)`;
-                quoteDisplay.text(quote);
-                quoteDisplay.attr('data-quote-id', id);
-                quoteDisplay.fadeIn();
-                setTimeout(() => {
-
-                }, 100);
-            });
-        }
-    }, changeQuoteInterval);
+    //Immediately change to a quote, then setInterval to slow down
+    changeQuote();
+    let timer = setInterval(changeQuote, changeQuoteInterval);
+    $('#quote-container').on('click', () => {
+        changeQuote();
+        //Reset timer countdown
+        clearInterval(timer);
+        timer = setInterval(changeQuote, changeQuoteInterval);
+    });
 })();
+
+/**
+ * If index is not specified or unavailable, select a random quote
+ * @param index
+ */
+function changeQuote(index ?: number) {
+    const currId = quoteDisplay.attr('data-quote-id');
+
+    const id = index < quotes.length ? index : Math.floor(Math.random() * quotes.length);
+    if (id.toString() !== currId) {
+        const quote = `${quotes[id].quote} - ${quotes[id].author}`;
+        quoteDisplay.fadeOut(() => {
+            quoteContainer.css('width', `min(${quote.length * 10}px, 80vw)`);
+            quoteDisplay.text(quote);
+            quoteDisplay.attr('data-quote-id', id);
+            quoteDisplay.fadeIn();
+        });
+    }
+}
