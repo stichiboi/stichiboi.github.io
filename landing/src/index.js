e54ece39067fe439fbeb35eb700d5c72562addef
stichiboi <stichiboi@gmail.com>
2021-02-08 09:10:43 +0100

    Initial commit - landing page!

---
+++ landing/src/index.js
@@ -0,0 +1,4 @@
+import './css/main.css'
+import './css/content.css'
+import './css/responsive.css'
+import './css/text.css'
---
+++ landing/webpack/webpack.common.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//TODO Implement CleanWebpack plugin
+
+const ASSET_PATH = process.env.ASSET_PATH || '';
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, './../src/index.js'),
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        publicPath: ASSET_PATH,
+        path: path.resolve(__dirname, '..'),
+    },
+    resolve: {extensions: ['.tsx', '.ts', '.jsx', '.js']},
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|webp|svg|zip)$/,
+                use: {
+                    //Use file-loader rather than url-loader because
+                    // inline svg don't display in older versions of Adobe CEP (17.1.3 and earlier)
+                    loader: "file-loader",
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets'
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.ts$/,
+                use: ['ts-loader']
+            },
+        ]
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Main UI',
+            filename: 'index.html',
+            template: path.resolve(__dirname, './../src/index.html'),
+            inlineSource: '.(js)$',
+            chunks: ['index']
+        }),
+        // new CleanWebpackPlugin(),
+    ]
+};
