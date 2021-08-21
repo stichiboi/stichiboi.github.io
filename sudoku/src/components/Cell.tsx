84adc7c93a08b11f59c3781b66d48d34494ed081
Daniele Mazzotta <stichiboi@gmail.com>
2021-08-21 11:09:12 +0200

    note display

--- sudoku/src/components/Cell.tsx
+++ sudoku/src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import {CELL_HIGHLIGHT, ICell} from "../types/types";
 
 export default function Cell({
@@ -6,11 +6,20 @@ export default function Cell({
                                  onClick,
                                  highlight
                              }: { cell: ICell, onClick: () => unknown, highlight?: CELL_HIGHLIGHT }) {
+
+    const formatNotes = useCallback((notes: Set<number>) => {
+        const components = [] as JSX.Element[];
+        notes.forEach(n => components.push((
+            <p key={n}>{n}</p>
+        )));
+        return components;
+    }, []);
+
     return (
         <button
-            className={`button-action fill ${highlight ? 'highlight-' + CELL_HIGHLIGHT[highlight].toLowerCase() : ''}`}
+            className={`button-action fill ${highlight ? 'highlight-' + CELL_HIGHLIGHT[highlight].toLowerCase() : ''} ${cell.isFixed ? '--fixed' : ''}`}
             onClick={onClick}>
-            {cell.value || cell.notes}
+            {cell.value || <div className={"cell-notes"}>{formatNotes(cell.notes)}</div>}
         </button>
     )
 }
